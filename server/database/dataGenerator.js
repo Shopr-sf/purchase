86bca357109318a645154ad7ce01152e8bd12568
John Lukenoff <jlukenoff@gmail.com>
2018-08-08 10:23:19 -0700

    Initial dataGen setup

---
+++ server/database/dataGenerator.js
@@ -0,0 +1,46 @@
+/* const sampleData = {
+  data: {
+    'id': 32,
+    'brand': 'GE',
+    name: 'Almond',
+    product_tier: 'Nano',
+    'product_options': {
+      'size': [
+        'S',
+        'M',
+        'L',
+        'XL',
+      ],
+      'color': [
+        'green',
+        'white',
+        'blue',
+        'black',
+        'silver',
+        'purple',
+      ],
+    },
+    price: {
+      list: 359.08,
+      'msrp': 398.98,
+      sale: 239.38,
+    },
+    about_product: [
+      ' Blog sartorial DIY, eu qui migas lomo poutine',
+      ' Raw denim hammock shoreditch do',
+      ' Adipisicing shabby chic velit authentic, asymmetrical aesthetic prism',
+    ],
+    is_prime: 0,
+    'stock_count': 1,
+    reviews: [
+      665,
+      214,
+      528,
+      162,
+      684,
+    ],
+    'questions': 0,
+    seller: 'Amazon',
+    thumbnail: 'Nano.jpg',
+  }
+}; */

58203d07be294c20b7ef12a27ebce7dad8dcd69b
John Lukenoff <jlukenoff@gmail.com>
2018-08-07 17:27:16 -0700

    Remove console.log

--- client/components/ProductInfo.jsx
+++ client/components/ProductInfo.jsx
@@ -89,7 +89,6 @@ class ProductInfo extends React.Component {
 
   get() {
     const { productId } = this.state;
-    console.log(productId);
     fetch(`/products${productId}`)
       .then(response => response.json())
       .then((obj) => {

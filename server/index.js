427426b74b273e143363f56487d2b72cb191b1e6
John Lukenoff <jlukenoff@gmail.com>
2018-08-10 14:15:11 -0700

    File restructure

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
-const database = require('./database.js');
+const database = require('./database/database.js');
 
 const app = express();
 

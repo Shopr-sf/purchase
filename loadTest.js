96fe343ef270156391fae8e4cb017c153f11e6fb
John Lukenoff <jlukenoff@gmail.com>
2018-08-16 11:36:37 -0700

    implement clustering and redis

--- loadTest.js
+++ loadTest.js
@@ -2,10 +2,16 @@ const siege = require('siege');
 
 const sieger = siege().on(3003);
 
-const renderWeightedId = () => 10000000 - Math.ceil(Math.random() * 2000000);
+const renderWeightedId = (callCount) => {
+  let max = 1000000;
+  if (callCount % 1000 === 0) {
+    max = 100;
+  } 
+  return 10000000 - Math.ceil(Math.random() * max);
+}
 
 for (let i = 0; i < 100000; i++) {
-  sieger.for(1).times.get(`/products/${renderWeightedId()}`);
+  sieger.for(1).times.get(`/products/${renderWeightedId(i)}`);
 }
 
 sieger.attack();
--- server/database/database.js
+++ server/database/database.js
@@ -5,27 +5,17 @@ const pool = new Pool({
   database: 'product_db',
   port: 5432,
 });
-// pool.connect();
 
 
-const getProduct = function getProductInformation(productId, callback) {
-  pool.query(`select * from products where id=${productId}`, (err, results) => {
-    if (err) console.error(err);
-    callback(results);
-  });
-};
-
-const getRelated = function getRelatedProducts(productType, productId, callback) {
-  pool.query(`select about_product, list_price, reviews from products where type=${productType} and id <> ${productId} limit 5`, (err, results) => {
-    if (err) console.error(err);
-    callback(results);
-  });
-};
-
-const getAll = function getProductAndRelatedProducts(productId, callback) { 
-  pool.query(`select t1.*, t2.about_product, t2.list_price, t2.reviews from products t1 inner join products t2 on t1.id=${productId} and t2.type=t1.type and t1.id!=t2.id limit 6`, (err, result) => {
+const getAll = function getProductAndRelatedProducts(productId, callback) {
+  const query = {
+    name: 'get-all',
+    text: 'select t1.*, t2.about_product, t2.list_price, t2.reviews from products t1 inner join products t2 on t1.id=$1 and t2.type=t1.type and t1.id!=t2.id limit 6',
+    values: [productId],
+  };
+  pool.query(query, (err, result) => {
     if (err) console.error(`Error fetching product data from postgres: ${err}`);
-    callback(result);
+    callback(result.rows);
   });
 };
 
@@ -39,14 +29,11 @@ const updateOne = function (params, cb) {
   pool.query(queryString, cb);
 };
 
-/* TODO: add DELETE functionality */
 const removeOne = function (id, cb) {
   pool.query(`DELETE from products WHERE id=${id}`, cb);
 };
 
 module.exports = {
-  getProduct,
-  getRelated,
   getAll,
   insertNew,
   updateOne,
